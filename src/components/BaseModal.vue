0e57754774ff51c465efb61341f3d17846a9f6b5
aaa2002 <alexand2002@gmail.com>
2023-07-20 20:01:14 +0300

    Commit #2

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -2,16 +2,17 @@
     <div v-show="modalActive" class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
         <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-medium">
             <slot />
-            <button class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+            <button class="text-white mt-8 bg-weather-primary py-2 px-6" @click="$emit('closeModal')">Close</button>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+defineEmits(['closeModal']);
 defineProps({
     modalActive: {
         type: Boolean,
         default: false,
     }
-})
+});
 </script>
