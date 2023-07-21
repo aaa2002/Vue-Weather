924765defbf077eb5e45b2a841a720fae9576a81
aaa2002 <alexand2002@gmail.com>
2023-07-21 17:49:50 +0300

    Commit #7 - Hourly Weather

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -27,69 +27,143 @@
                 {{ Math.round(weatherData.current_weather.temperature) }}&deg;C
             </p>
 
-            <div class="text-center">
-                <p>
-                    Wind speed:
-                    {{ weatherData.current_weather.windspeed }} km/h
-                </p>
-                <p>
-                    Chances of rain:
-                    {{ precip }}%
-                </p>
-                <i v-if="weatherCode === 0" class="fa-solid fa-sun p-6 text-6xl"></i>
-                <i v-if="weatherCode === 1" class="fa-solid fa-sun p-6 text-6xl"></i>
-                <i v-if="weatherCode === 2" class="fa-solid fa-cloud-sun p-6 text-6xl"></i>
-                <i v-if="weatherCode === 3" class="fa-solid fa-cloud p-6 text-6xl"></i>
+            <p>
+                Wind speed:
+                {{ weatherData.current_weather.windspeed }} km/h
+            </p>
+            <p>
+                Chances of rain:
+                {{ precip }}%
+            </p>
+            <i v-if="weatherCode === 0" class="fa-solid fa-sun p-6 text-6xl"></i>
+            <i v-if="weatherCode === 1" class="fa-solid fa-sun p-6 text-6xl"></i>
+            <i v-if="weatherCode === 2" class="fa-solid fa-cloud-sun p-6 text-6xl"></i>
+            <i v-if="weatherCode === 3" class="fa-solid fa-cloud p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 45" class="fa-solid fa-smog p-6 text-6xl"></i>
-                <i v-if="weatherCode === 48" class="fa-solid fa-smog p-6 text-6xl"></i>
+            <i v-if="weatherCode === 45" class="fa-solid fa-smog p-6 text-6xl"></i>
+            <i v-if="weatherCode === 48" class="fa-solid fa-smog p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 51" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 53" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 55" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 51" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 53" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 55" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 56" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 56" class="fa-solid fa-snowflake p-6 text-6xl"></i>
-                <i v-if="weatherCode === 57" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 57" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+            <i v-if="weatherCode === 56" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 56" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+            <i v-if="weatherCode === 57" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 57" class="fa-solid fa-snowflake p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 61" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 63" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 65" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 61" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 63" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 65" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 66" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 66" class="fa-solid fa-snowflake p-6 text-6xl"></i>
-                <i v-if="weatherCode === 67" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 67" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+            <i v-if="weatherCode === 66" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 66" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+            <i v-if="weatherCode === 67" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 67" class="fa-solid fa-snowflake p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 71" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
-                <i v-if="weatherCode === 73" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
-                <i v-if="weatherCode === 75" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+            <i v-if="weatherCode === 71" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+            <i v-if="weatherCode === 73" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+            <i v-if="weatherCode === 75" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 77" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+            <i v-if="weatherCode === 77" class="fa-solid fa-snowflake p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 80" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 81" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
-                <i v-if="weatherCode === 82" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 80" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 81" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+            <i v-if="weatherCode === 82" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 85" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
-                <i v-if="weatherCode === 86" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+            <i v-if="weatherCode === 85" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+            <i v-if="weatherCode === 86" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 95" class="fa-solid fa-cloud-bolt p-6 text-6xl"></i>
+            <i v-if="weatherCode === 95" class="fa-solid fa-cloud-bolt p-6 text-6xl"></i>
 
 
-                <i v-if="weatherCode === 96" class="fa-solid fa-cloud-meatball p-6 text-6xl"> Slight Hail</i>
-                <i v-if="weatherCode === 99" class="fa-solid fa-cloud-meatball p-6 text-6xl"> Heavy Hail</i>
+            <i v-if="weatherCode === 96" class="fa-solid fa-cloud-meatball p-6 text-6xl"> Slight Hail</i>
+            <i v-if="weatherCode === 99" class="fa-solid fa-cloud-meatball p-6 text-6xl"> Heavy Hail</i>
+
+        </div>
+
+        <hr class="border-white border-opacity-10 border w-full" />
+
+
+        <!-- Hourly Weather -->
+
+        <div class="max-w-screen-medium w-full py-12">
+            <div class="mx-8 text-white">
+                <h2 class="mb-4">Hourly Weather</h2>
+                <div class="flex gap-10 overflow-x-scroll">
+                    <div v-for="hourData in weatherData.hourly.time" :key="hourData"
+                        class="flex flex-col gap-4 items-center">
+                        <p class="whitespace-nowrap text-md">
+                            {{ new Date(hourData).toLocaleTimeString("en-us", {
+                                hour: "numeric"
+                            }) }}
+                        </p>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 0" class="fa-solid fa-sun p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 1" class="fa-solid fa-sun p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 2" class="fa-solid fa-cloud-sun p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 3" class="fa-solid fa-cloud p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 45" class="fa-solid fa-smog p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 48" class="fa-solid fa-smog p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 51" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 53" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 55" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 56" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 56" class="fa-solid fa-snowflake p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 57" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 57" class="fa-solid fa-snowflake p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 61" class="fa-solid fa-cloud-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 63" class="fa-solid fa-cloud-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 65" class="fa-solid fa-cloud-rain p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 66" class="fa-solid fa-cloud-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 66" class="fa-solid fa-snowflake p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 67" class="fa-solid fa-cloud-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 67" class="fa-solid fa-snowflake p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 71" class="fa-solid fa-cloud-meatball p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 73" class="fa-solid fa-cloud-meatball p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 75" class="fa-solid fa-cloud-meatball p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 77" class="fa-solid fa-snowflake p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 80" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 81" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 82" class="fa-solid fa-cloud-sun-rain p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 85" class="fa-solid fa-cloud-meatball p-3 text-3xl"></i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 86" class="fa-solid fa-cloud-meatball p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 95" class="fa-solid fa-cloud-bolt p-3 text-3xl"></i>
+
+
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 96" class="fa-solid fa-cloud-meatball p-3 text-3xl"> Slight Hail</i>
+                        <i v-if="weatherData.hourly.weathercode[weatherData.hourly.time.indexOf(hourData)] === 99" class="fa-solid fa-cloud-meatball p-3 text-3xl"> Heavy Hail</i>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -105,7 +179,7 @@ const getWeatherData = async () => {
         const latitude = route.query.latitude;
         const longitude = route.query.longitude;
         console.log(latitude, longitude)
-        const weatherData = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,apparent_temperature,precipitation_probability,is_day&current_weather=true`);
+        const weatherData = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,is_day&current_weather=true`);
 
 
 
@@ -136,5 +210,5 @@ const getCurrent = () => {
 
 const precip = weatherData.hourly.precipitation_probability[getCurrent()];
 const weatherCode = weatherData.current_weather.weathercode;
-console.log(precip, weatherCode);
+console.log(precip, weatherCode, weatherData.hourly.time);
 </script>
