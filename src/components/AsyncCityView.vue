dc8fcef8d842ef47fd6681b70bb590b6e45b082b
aaa2002 <alexand2002@gmail.com>
2023-07-21 17:14:11 +0300

    Commit #66 - Location preview page

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,6 +1,97 @@
 <template>
-    <div>
+    <div class="flex flex-col flex-1 items-center">
+        <!-- Banner -->
+        <div v-if="route.query.preview" class="text-white p-4 bg-weather-secondary w-full text-center">
+            <p>
+                You are currently previewing this city, click the "+"
+                icon to start tracking this location.
+            </p>
+        </div>
 
+        <!-- Weather Overview -->
+
+        <div class="flex flex-col items-center text-white py-12">
+            <h1 class="text-4xl mb2">{{ route.params.location }}</h1>
+            <p class="text-sm mb-12">
+                {{ new Date(weatherData.current_weather.time).toLocaleDateString("en-US", {
+                    weekday: "short",
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric"
+                }) }}
+                {{ new Date(weatherData.current_weather.time).toLocaleTimeString("en-US", {
+                    timeStyle: "short"
+                }) }}
+            </p>
+            <p class="text-8xl mb-8">
+                {{ Math.round(weatherData.current_weather.temperature) }}&deg;C
+            </p>
+
+            <div class="text-center">
+                <p>
+                    Wind speed:
+                    {{ weatherData.current_weather.windspeed }} km/h
+                </p>
+                <p>
+                    Chances of rain:
+                    {{ precip }}%
+                </p>
+                <i v-if="weatherCode === 0" class="fa-solid fa-sun p-6 text-6xl"></i>
+                <i v-if="weatherCode === 1" class="fa-solid fa-sun p-6 text-6xl"></i>
+                <i v-if="weatherCode === 2" class="fa-solid fa-cloud-sun p-6 text-6xl"></i>
+                <i v-if="weatherCode === 3" class="fa-solid fa-cloud p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 45" class="fa-solid fa-smog p-6 text-6xl"></i>
+                <i v-if="weatherCode === 48" class="fa-solid fa-smog p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 51" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 53" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 55" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 56" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 56" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+                <i v-if="weatherCode === 57" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 57" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 61" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 63" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 65" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 66" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 66" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+                <i v-if="weatherCode === 67" class="fa-solid fa-cloud-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 67" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 71" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+                <i v-if="weatherCode === 73" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+                <i v-if="weatherCode === 75" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 77" class="fa-solid fa-snowflake p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 80" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 81" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+                <i v-if="weatherCode === 82" class="fa-solid fa-cloud-sun-rain p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 85" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+                <i v-if="weatherCode === 86" class="fa-solid fa-cloud-meatball p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 95" class="fa-solid fa-cloud-bolt p-6 text-6xl"></i>
+
+
+                <i v-if="weatherCode === 96" class="fa-solid fa-cloud-meatball p-6 text-6xl"> Slight Hail</i>
+                <i v-if="weatherCode === 99" class="fa-solid fa-cloud-meatball p-6 text-6xl"> Heavy Hail</i>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,11 +105,14 @@ const getWeatherData = async () => {
         const latitude = route.query.latitude;
         const longitude = route.query.longitude;
         console.log(latitude, longitude)
-        const weatherData = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+        const weatherData = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,apparent_temperature,precipitation_probability,is_day&current_weather=true`);
 
-        
 
-        return weatherData;
+
+        console.log(weatherData.data.current_weather.time);
+        console.log(weatherData.data.current_weather.temperature);
+
+        return weatherData.data;
     } catch (err) {
         console.log(err);
     }
@@ -27,4 +121,20 @@ const getWeatherData = async () => {
 
 const weatherData = await getWeatherData();
 console.log(weatherData);
+
+console.log(weatherData.hourly.precipitation_probability);
+
+const getCurrent = () => {
+
+    const index = weatherData.hourly.time.findIndex(
+        (element) => new Date(element).getTime() === new Date(weatherData.current_weather.time).getTime()
+    );
+    return index !== -1 ? index : 0;
+
+}
+
+
+const precip = weatherData.hourly.precipitation_probability[getCurrent()];
+const weatherCode = weatherData.current_weather.weathercode;
+console.log(precip, weatherCode);
 </script>
